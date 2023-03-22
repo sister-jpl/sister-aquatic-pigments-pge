d2cab83958fbab10b4425ae281e816657abdd3dd
danyu <danyu@jpl.nasa.gov>
2023-03-22 13:32:08 -0700

    changed python=3.7.16 for phycocyanin

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ conda deactivate
 popd
 
 # Create conda environment for phycocyanin
-conda create -n ap-phyco -y python=3.7 gdal
+conda create -n ap-phyco -y python=3.7.16 gdal
 source activate ap-phyco
 
 pushd sister-mdn_phycocyanin
