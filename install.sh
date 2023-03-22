3fb421150129e179b890f9b257d5d62ee81c14d8
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2023-03-22 13:13:55 -0700

    Create two conda envs, one for each pigment

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 pge_dir=$(cd "$(dirname "$0")" ; pwd -P)
 app_dir=$(dirname ${pge_dir})
 
-cd $app_dir
+pushd $app_dir
 git clone https://github.com/EnSpec/sister-mdn_chlorophyll.git -b master
 git clone https://github.com/EnSpec/sister-mdn_phycocyanin.git -b main
 
-# Create conda environment
-conda create -n aquatic-pigments -y -c conda-forge python=3.8 gdal
-source activate aquatic-pigments
+# Create conda environment for chlorophyll-a
+conda create -n ap-chla -y python=3.8 gdal
+source activate ap-chla
 
-cd sister-mdn_chlorophyll
+pushd sister-mdn_chlorophyll
 pip install -e .
 
-cd ../sister-mdn_phycocyanin
+conda deactivate
+popd
+
+# Create conda environment for phycocyanin
+conda create -n ap-phyco -y python=3.7 gdal
+source activate ap-phyco
+
+pushd sister-mdn_phycocyanin
 pip install -e .
