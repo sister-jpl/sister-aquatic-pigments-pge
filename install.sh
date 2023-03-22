432467aab15b1c56078edd4d8a9e61ad64959c0c
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2023-03-22 14:24:22 -0700

    Install awscli

--- install.sh
+++ install.sh
@@ -1,25 +1,24 @@
 pge_dir=$(cd "$(dirname "$0")" ; pwd -P)
 app_dir=$(dirname ${pge_dir})
 
+# Clone the repositories into the app directory
+
 pushd $app_dir
 git clone https://github.com/EnSpec/sister-mdn_chlorophyll.git -b master
 git clone https://github.com/EnSpec/sister-mdn_phycocyanin.git -b main
 
-# Copy model weights file to chlorophyll
-
-aws cp http://s3//sister-ops-registry/packages/mdn_chlorophyll_weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip sister-mdn_chlorophyll/MDN/Weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip
-
-# Create conda environment for chlorophyll-a
-conda create -n ap-chla -y python=3.8 gdal
+# Create conda environment for chlorophyll-a and install
+conda create -n ap-chla -y python=3.8 gdal awscli
 source activate ap-chla
 
 pushd sister-mdn_chlorophyll
+aws cp s3://sister-ops-registry/packages/mdn_chlorophyll_weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip MDN/Weights/HICO/
 pip install -e .
 
 conda deactivate
 popd
 
-# Create conda environment for phycocyanin
+# Create conda environment for phycocyanin and install
 conda create -n ap-phyco -y python=3.7.16 gdal
 source activate ap-phyco
 
