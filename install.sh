6d0307fba95b6f63857dd1de29fe997931d492f8
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2023-03-23 08:47:17 -0700

    Minor tweaks tested locally

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ git clone https://github.com/EnSpec/sister-mdn_chlorophyll.git -b master
 git clone https://github.com/EnSpec/sister-mdn_phycocyanin.git -b main
 
 # Create conda environment for chlorophyll-a and install
-conda create -n ap-chla -y -c conda-forge python=3.8 gdal=3 awscliv2=2
+conda create -n ap-chla -y -c conda-forge python=3.8 gdal=3 awscli=1
 source activate ap-chla
 
 pushd sister-mdn_chlorophyll
-aws s3 cp s3://sister-ops-registry/packages/mdn_chlorophyll_weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip MDN/Weights/HICO/
+aws s3 cp s3://sister-ops-registry/packages/mdn_chlorophyll_weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip MDN/Weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip
 pip install -e .
 
 conda deactivate
 popd
 
 # Create conda environment for phycocyanin and install
-conda create -n ap-phyco -y -c conda-forge python=3.7.16 gdal=3
+conda create -n ap-phyco -y python=3.7.16 gdal=3
 source activate ap-phyco
 
 pushd sister-mdn_phycocyanin
